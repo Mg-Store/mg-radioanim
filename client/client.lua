24981e3bdfb0249ef66ae9aa9b3493943998e096
Mg Store <fivemmgstore@gmail.com>
2025-03-22 23:50:48 +0300

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,70 +1,44 @@
-Config ={}
-local SelectAnimDict = "anim@cop_mic_pose_001"
-local SelectAnimName = "chest_mic"
-local radioProp;
+fx_version "adamant"
 
-RegisterCommand('radioanim', function()
-    local animListMenu = {}
+description "Mg Store | mg-radioanim"
+author "MG"
+version "1.0.0"
+lua54 "yes"
+game "gta5"
 
-    animListMenu[#animListMenu + 1] = {
-        title = Lang:t('menu.title'),
-        description = Lang:t('menu.description'),
-        disabled = true 
-    }
+escrow_ignore { 
+    "Settings.lua"
+}
 
-    for k, v in ipairs(Config.animList) do
-        animListMenu[#animListMenu + 1] = {
-            title = v.AnimTitle, 
-            icon = 'fa-solid fa-radio', 
-            description = '', 
-            event = 'mg:radioanim:selectAnim', 
-            args = {
-                AnimTitle = v.AnimTitle,
-                AnimDict = v.AnimDict,
-                AnimName = v.AnimName
-            },
-            image = v.image
-        }
-    end
+shared_script {
+    '@qb-core/shared/locale.lua',
+    '@ox_lib/init.lua',
+    'locales/en.lua'
+}
 
-    lib.registerContext({
-        id = 'radio_anim_menu',
-        title = Lang:t('menu.main'),
-        options = animListMenu 
-    })
+client_script {
+    'client/client.lua',
+    'Settings.lua'
+}
 
-    lib.showContext('radio_anim_menu')
-end)
+server_script {
+    'server/server.lua'
+}
 
-RegisterNetEvent('mg:radioanim:selectAnim', function(data)
-    SelectAnimDict= data.AnimDict
-    SelectAnimName = data.AnimName
-end)
+files {
+    "stream/*.ycd",
+    "assets/images/*.png"  -- Resim dosyalarını burada ekliyoruz
+}
 
-RegisterNetEvent('mg:radioanim:animPlay', function()
-    local playerPed = PlayerPedId()
-    if SelectAnimName == "holding_radio_clip" then 
-        SetCurrentPedWeapon(playerPed, GetHashKey('WEAPON_UNARMED'), true)
-        radioProp = CreateObject(`prop_cs_hand_radio`, 1.0, 1.0, 1.0, 1, 1, 0)
-        AttachEntityToEntity(radioProp, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 57005), 0.13544876595097, 0.051973119787891, -0.029258303514433, -93.78105269127, -7.4997158143199, -36.431763377751, true, true, false, true, 1, true)
-    elseif SelectAnimName == "radio_left_clip" then 
-        SetCurrentPedWeapon(playerPed, GetHashKey('WEAPON_UNARMED'), true)
-        radioProp = CreateObject(`prop_cs_hand_radio`, 1.0, 1.0, 1.0, 1, 1, 0)
-        AttachEntityToEntity(radioProp, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 18905), 0.13544876595097, 0.051973119787891, 0.029258303514433, -93.78105269127, -7.4997158143199, -36.431763377751, true, true, false, true, 1, true)
-    end
-    local dict = SelectAnimDict
-    local anim = SelectAnimName
-    RequestAnimDict(dict)
-    while not HasAnimDictLoaded(dict) do
-        Wait(100)
-    end
-    TaskPlayAnim(playerPed, dict, anim, 8.0, -8.0, -1, 49, 0, false, false, false)
-end)
-
-RegisterNetEvent('mg:radioanim:animStop', function()
-    local playerPed = PlayerPedId()
-    ClearPedTasks(playerPed)
-    if SelectAnimName == "holding_radio_clip" or SelectAnimName == "radio_left_clip" then 
-        DeleteObject(radioProp)
-    end
-end)
+data_file 'ANIM_DICT' 'stream/anim@cop_radio_pose.ycd'
+data_file 'ANIM_DICT' 'stream/anim@hand_rest_holster.ycd'
+data_file 'ANIM_DICT' 'stream/anim@cop_pose_escorting.ycd'
+data_file 'ANIM_DICT' 'stream/anim@hand_rest_belt.ycd'
+data_file 'ANIM_DICT' 'stream/anim@cop_pose.ycd'
+data_file 'ANIM_DICT' 'stream/anim@cop_mic_pose_001.ycd'
+data_file 'ANIM_DICT' 'stream/anim@cop_mic_pose_002.ycd'
+data_file 'ANIM_DICT' 'stream/anim@cop_mic_pose_002_1.ycd'
+data_file 'ANIM_DICT' 'stream/anim@cop_mic_pose_002_2.ycd'
+data_file 'ANIM_DICT' 'stream/anim@belt_pose_hlstr.ycd'
+data_file 'ANIM_DICT' 'stream/anim@male@holding_radio.ycd'
+data_file 'ANIM_DICT' 'stream/anim@radio_left.ycd'
